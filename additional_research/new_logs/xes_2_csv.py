5b141f7245ef451b2551ecee53ded38dc75a7429
Babett Müller <pepsix3@web.de>
2024-05-08 15:36:15 +0200

    new new_logs

--- additional_research/new_logs/xes_2_csv.py
+++ additional_research/new_logs/xes_2_csv.py
@@ -1,5 +1,5 @@
 import pm4py
 
-log = pm4py.read_xes('Hospital_log.xes')
+log = pm4py.read_xes(r'additional_research\new_logs\Hospital_log.xes.gz')
 pd = pm4py.convert_to_dataframe(log)
-pd.to_csv('Hospital_log.csv')
+pd.to_csv(r'additional_research\new_logs\Hospital_log.csv')
