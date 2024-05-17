acc6676ef64a3a0520db26e6ec4b8075e1665ce1
Babett Müller <pepsix3@web.de>
2024-05-17 17:06:04 +0200

    delete scirpt

--- additional_research/new_logs/xes_2_csv.py
+++
@@ -1,5 +0,0 @@
-import pm4py
-
-log = pm4py.read_xes(r'additional_research\new_logs\Hospital_log.xes.gz')
-pd = pm4py.convert_to_dataframe(log)
-pd.to_csv(r'additional_research\new_logs\Hospital_log.csv')
